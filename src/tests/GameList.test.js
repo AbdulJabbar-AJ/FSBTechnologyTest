27994baae01aa4abac874cb9c88f053f8265b2b7
Abdul Jabbar <abdulabase@gmail.com>
2020-03-18 09:45:26 +0000

    minor change

--- src/tests/GameList.test.js
+++ src/tests/GameList.test.js
@@ -1,3 +1,8 @@
+import React from 'react'
+import Enzyme, { shallow, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+Enzyme.configure({ adapter: new Adapter() })
+
 test('Fake Test', () => {
     expect(true).toBeTruthy()
 })
